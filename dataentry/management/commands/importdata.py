4b0d7f4fd89c7430093289196358e257252ab344
Aqeeb Ansari <aqeeb206@gmail.com>
2025-11-19 15:24:05 +0530

    import data from the csv

---
+++ dataentry/management/commands/importdata.py
@@ -0,0 +1,58 @@
+from django.core.management.base import BaseCommand,CommandError
+# from dataentry.models import Student
+from django.apps import apps
+import csv
+# #proposed command -python manage.py importdata file_path
+# class Command(BaseCommand):
+
+#     help='inserting/importing data from csv file'
+
+#     def add_arguments(self, parser):
+#         parser.add_argument('filepath',type=str,help='Path to the csv file')
+        
+#     def handle(self, *args, **kwargs):
+#         #logic goes here
+#         file_path=kwargs['filepath']
+#         with open(file_path,'r') as file:
+#             reader=csv.DictReader(file)
+#             for row in reader:
+#                 Student.objects.create(**row)
+      
+#         self.stdout.write(self.style.SUCCESS('Data imported from csv successfully'))
+
+# import data to any model 
+
+#proposed command -python manage.py importdata file_path model_name
+class Command(BaseCommand):
+
+    help='inserting/importing data from csv file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filepath',type=str,help='Path to the csv file')
+        parser.add_argument('model_name', type=str, help='Name of the Model')
+
+        
+    def handle(self, *args, **kwargs):
+        #logic goes here
+        file_path=kwargs['filepath']
+        model_name=kwargs['model_name'].capitalize()
+
+        #Search for models across all installed apps
+        model=None
+        for app_config in apps.get_app_configs():
+            #Try to search for the model 
+            try:
+                model=apps.get_model(app_config.label,model_name)
+                break # stop searching once the model is found
+            except LookupError:
+                continue # model not found in this app, continue searching in next app.
+
+        if not model:
+            raise CommandError(f"Model '{model_name}' not found in any app'!")
+
+        with open(file_path,'r') as file:
+            reader=csv.DictReader(file)
+            for row in reader:
+                model.objects.create(**row)
+      
+        self.stdout.write(self.style.SUCCESS('Data imported from csv successfully'))
---
+++ dataentry/management/commands/insertdata.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from dataentry.models import Student
+
+# I want to add some data to database using the custom command
+
+class Command(BaseCommand):
+    help='It will insert data into the database'
+
+    def handle(self, *args, **options):
+        #logic for inserting goes here
+        dataset=[
+            {'roll_no':1002, 'name': 'Sachin', 'age': 24},
+            {'roll_no':1006, 'name': 'Michael', 'age': 29},
+            {'roll_no':1004, 'name': 'Mike', 'age': 20},
+            {'roll_no':1005, 'name': 'Joseph', 'age': 26},
+        ]
+
+        for data in dataset:
+            roll_no=data['roll_no']
+            existing_record=Student.objects.filter(roll_no=roll_no).exists()
+
+            if not existing_record:
+                Student.objects.create(roll_no=data['roll_no'],name=data['name'],age=data['age'])
+            else:
+                self.stdout.write(self.style.WARNING(f'Student with {roll_no} already exists!'))
+
+        self.stdout.write(self.style.SUCCESS('Data inserted successfully'))
+
