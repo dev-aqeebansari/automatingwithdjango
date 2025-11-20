6782cb625cd3a903f216ee08aa88e067f761c2b8
Aqeeb Ansari <aqeeb206@gmail.com>
2025-11-20 12:05:44 +0530

    export data command

---
+++ dataentry/management/commands/exportdata.py
@@ -0,0 +1,86 @@
+from django.core.management.base import BaseCommand
+# from dataentry.models import Student
+from django.apps import apps
+import datetime
+import csv
+
+# #proposed command = python manage.py exportdata 
+# class Command(BaseCommand):
+    
+#     help="Export data from Student model to a CSV file"
+
+#     def handle(self, *args, **kwargs):
+#         #fetch the data from the database
+#         students=Student.objects.all()
+
+#         #generate the timestamp of current date and time 
+#         timestamp=datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+#         # define the csv file name/path
+#         file_path=f'exported_students_data_{timestamp}.csv'
+
+#         # open the csv file and write the data 
+#         with open(file_path,'w', newline='') as file:
+#             writer=csv.writer(file)
+
+
+#             # write the csv header
+#             writer.writerow(['Roll No','Name', 'Age'])
+
+#             #write data rows
+#             for student in students:
+#                 writer.writerow([student.roll_no, student.name, student.age])
+        
+#         self.stdout.write(self.style.SUCCESS('Data exported successfully'))
+
+
+#proposed command = python manage.py exportdata model_name
+class Command(BaseCommand):
+    help="Export data from  the database  to a CSV file"
+
+
+    def add_arguments(self, parser):
+        parser.add_argument('model_name', type=str, help='Model name')
+
+
+    def handle(self, *args, **kwargs):
+
+        
+        model_name=kwargs['model_name'].capitalize()
+
+        #search through all the installed apps for the model
+        model=None
+        for app_config in apps.get_app_configs():
+            try:
+                model=apps.get_model(app_config.label,model_name)
+                break # stop executing once the model is found
+            except LookupError:
+                pass
+        
+        if not model:
+            self.stderr.write(f'Model {model_name} could not found')
+            return
+        
+        #fetch the data from the database
+        data=model.objects.all()
+
+        #generate the timestamp of current date and time 
+        timestamp=datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+        # define the csv file name/path
+        file_path=f'exported_{model_name}_data_{timestamp}.csv'
+
+        # open the csv file and write the data 
+        with open(file_path,'w', newline='') as file:
+            writer=csv.writer(file)
+
+
+            # write the csv header
+            # we want to print the field of the model that we are trying to export
+            writer.writerow([ field.name for field in model._meta.fields])
+
+            #write data rows
+            for dt in data:
+                writer.writerow([getattr(dt, field.name) for field in model._meta.fields])
+        
+        self.stdout.write(self.style.SUCCESS('Data exported successfully'))
