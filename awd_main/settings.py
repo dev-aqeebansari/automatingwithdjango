6a99da587cdf4b3723413a7ad47374e7ac82f265
Aqeeb Ansari <aqeeb206@gmail.com>
2025-11-23 08:51:51 +0530

    import data using the frontend

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dataentry',
+    'uploads',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'awd_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media Files Configuration
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    50: "critical",
+}
--- dataentry/urls.py
+++ dataentry/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
+from dataentry import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
-    path('',include('dataentry.urls')),
-] + static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+    path('import-data/',views.import_data,name='import_data'),
+    
+]
